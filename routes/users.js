36aed2b2bc7565d7aa19ff6f0aea57cf8ba3382f
Ramiro Meneses <rmenses@salesforce.com>
2020-01-27 17:29:38 -0300

    users registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const uuid = require('uuid/v4');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+const pool = require('../database');
 
-router.get('/', (req, res) => res.send('users'));
+router.get('/', async (req, res) => {
+    try {
+        const response = await pool.query('SELECT * FROM users');
+        await pool.end;
+
+        res.send(response.rows);
+    } catch (error) {
+        res.send('Error');
+    }
+});
+
+router.post('/register', (req, res) => {
+    const {username, password, email} = req.body;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+            pool.query(`INSERT INTO users VALUES('${uuid()}', '${username}', '${email}', '${hash}')`)
+            .then(response =>{
+                    res.status(200).send({
+                        message: 'User created',
+                        response
+                    });
+                }
+            ).catch(err => res.status(400).send(err));
+
+        })
+    })
+});
 
 module.exports = router;
