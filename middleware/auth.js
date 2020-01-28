d9d72616530d00ec9171d1c5ea0da43b56487408
Ramiro Meneses <rmenses@salesforce.com>
2020-01-28 13:31:08 -0300

    Authentication with jwt

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const { TokenPrefix } = require('../keys');
+
+const authRequired = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== undefined){
+        const bearer = bearerHeader.split(" ");
+        
+        if(bearer[0] !== TokenPrefix){
+            res.sendStatus(401);
+        } else {
+            const bearerToken = bearer[0];
+
+            req.token = bearerToken;
+            next();
+        }
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { authRequired };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const uuid = require('uuid/v4');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const pool = require('../database');
+const jwt = require('jsonwebtoken');
+
+const { SecretKey } = require('../keys');
+const { authRequired } = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -14,6 +18,62 @@ router.get('/', async (req, res) => {
         res.send('Error');
     }
 });
+
+router.post('/verifyToken', authRequired, (req, res) => {
+    jwt.verify(req.headers['token'], SecretKey, (err, data) => {
+        if(err){
+            res.send(err);
+            return;
+        }
+        res.send(data);
+    })
+})
+
+router.post('/login', async (req, res) => {
+    const { userEmail, userPassword } = req.body;
+    
+    try {
+        const response = await pool.query(`SELECT email, username, password FROM users WHERE email = '${userEmail}'`);
+        await pool.end;
+
+        if(!response.rows.length) res.status(400).send({error: 'email not found'});
+
+        const { email, username, password } = response.rows[0];
+
+        bcrypt.compare(userPassword, password).then(match => {
+            if(!match) {
+                res.status(400).send({
+                    success: false,
+                    error: 'Incorrect Password'
+                });
+                return;
+            }
+
+            //login logic
+
+            const jwtPayload = {
+                email,
+                username
+            }
+
+            jwt.sign(jwtPayload, SecretKey, (err, token) => {
+                if(err){
+                    res.status(400).send(err);
+                    return;
+                }
+
+                res.status(200).send({
+                    success: true,
+                    token
+                });
+            })
+
+        }).catch(err => res.send(err));
+    } catch (error) {
+        console.error(error);
+    }
+    
+})
 
 router.post('/register', (req, res) => {
     const {username, password, email} = req.body;
