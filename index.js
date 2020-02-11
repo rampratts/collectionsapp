b2503c4c8a55edaadf22d55c4f89c0b061623799
Ramiro Meneses <rmenses@salesforce.com>
2020-02-11 14:08:51 -0300

    Changed default port to 5000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const pool = require('./database');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
